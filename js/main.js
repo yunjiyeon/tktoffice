674d25beb714c8f2a78846999c61fda7e29122e8
yunjiyeon <yangdom@naver.com>
2022-11-29 07:11:44 +0900

    피드

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /****** gnb sub menu slide ******/
 $('.btnSub').click(function(){
   $('#gnb').removeClass('hide');
-  $('#gnb').animate({right:0,left:0},300,'swing'); // swing 속도감이 높아짐 
+  $('#gnb').animate({right:0,left:0},300,'swing'); // swing 속도감이 높아짐
 });
 
 $('.close').click(function(){
@@ -10,6 +10,7 @@ $('.close').click(function(){
     $(this).addClass('hide');
   });
 });
+
 
 /****** banner slide ******/
 let bannerSlide = {
@@ -69,40 +70,7 @@ $btn.click(function(){
 });
 
 
-
-// $('.tktWrap').isotope({
-//   // options
-//   itemSelector: '.all',
-//   stagger:100,
-//   transitionDuration: '0.2s',
-//   hiddenStyle: {
-//     opacity: 0
-//   },
-//   visibleStyle: {
-//     opacity: 1
-//   }
-// });
-
-// $('.btnAll').click(function(){
-//   $('.tktWrap').isotope({ filter: '*' });
-// })
-// $('.btnPlay').click(function(){
-//   $('.tktWrap').isotope({ filter: '.play' });
-// })
-// $('.btnMusical').click(function(){
-//   $('.tktWrap').isotope({ filter: '.musical' });
-// })
-// $('.btnConcert').click(function(){
-//   $('.tktWrap').isotope({ filter: '.concert' });
-// })
-// $('.btnExhibit').click(function(){
-//   $('.tktWrap').isotope({ filter: '.exhibit' });
-// })
-
-
-
-/******* feed ******/
-
+/******* main feed ******/
 window.addEventListener("load", () => {
   //Masonry 레이아웃(Isotope 플러그인 이용)
   const iso = new Isotope(".feedList", {
@@ -133,6 +101,38 @@ window.addEventListener("load", () => {
   }
 });
 
+/******* feed ******/
+window.addEventListener("load", () => {
+  //Masonry 레이아웃(Isotope 플러그인 이용)
+  const iso = new Isotope(".s_feedList", {
+    // options
+    itemSelector: "article",
+  });
+
+  const filterBtn = document.querySelectorAll(".s_feedBtn>li"); //.btn>li들을 변수에
+
+  for (let el of filterBtn) {
+    //배열 filterBtn 의 아이템(갯수) 만큼 반복
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      //클릭을 할때 각 아이템(버튼)에 반복, on클라스 없애줌
+      for (let el of filterBtn) {
+        el.classList.remove("on");
+      }
+
+      //클릭한 버튼에 클라스 넣어줌
+      e.currentTarget.classList.add("on");
+
+      //클릭한 버튼에 있는 a태그 안의 속성 href의 value값을 가져온다
+      const filerFeed = e.currentTarget.querySelector("a").getAttribute("href");
+
+      iso.arrange({ filter: filerFeed }); //버튼을 누르면 필터링 작동(플러그인)
+    });
+  }
+});
+
+
 
 
 
@@ -140,7 +140,7 @@ window.addEventListener("load", () => {
 $(function(){
   $(window).scroll(function(){
     if ($(this).scrollTop() > 520){
-      $('.topBtn').addClass('topBtnActive').css('opacity','0.8');
+      $('.topBtn').addClass('topBtnActive').css('opacity','0.85');
       $('.fixedBtn').addClass('fixedBtnActive');
     }else{
       $('.topBtn').removeClass('topBtnActive').css('opacity','0');
@@ -156,40 +156,59 @@ $(function(){
     return false;
   });
 
+/**** 서브 메뉴 버튼 실행 *****/
+
+$("#allBtn").click(function(){
+  let x = document.getElementsByClassName("menuBtnTxt")[0];
+   x.innerText="전체"; 
+});
+$("#playBtn").click(function(){
+  let x = document.getElementsByClassName("menuBtnTxt")[0];
+   x.innerText="연극"; 
+});
+$("#musicalBtn").click(function(){
+  let x = document.getElementsByClassName("menuBtnTxt")[0];
+   x.innerText="뮤지컬"; 
+});
+$("#concertBtn").click(function(){
+  let x = document.getElementsByClassName("menuBtnTxt")[0];
+   x.innerText="콘서트"; 
+});
+$("#exhibitBtn").click(function(){
+  let x = document.getElementsByClassName("menuBtnTxt")[0];
+   x.innerText="전시/행사"; 
+});
+
+
+$("#tktRateBtn").click(function(){
+  let x = document.getElementsByClassName("orderBtnTxt")[0];
+   x.innerText="예매율순"; 
+});
+$("#latestBtn").click(function(){
+  let x = document.getElementsByClassName("orderBtnTxt")[0];
+   x.innerText="최신순"; 
+});
+$("#alphabetBtn").click(function(){
+  let x = document.getElementsByClassName("orderBtnTxt")[0];
+   x.innerText="가나다순"; 
+});
 
 
 
-
-// const $popupMenu = $('.s_popup li a');  //버튼역할하는 nav a를 변수로
-// const 
-// $popupMenu.click(function(){
-//   $('.s_tktMenu #s_tktMenuBtn span').removeClass('on');
-//   $('.s_tktMenu #s_tktMenuBtn span').addClass('on');
-
-// });
-
-
-
-
-
-  /**** 서브 메뉴 분야 선택 *****/
-
+  /**** 서브 메뉴 실행 *****/
 $('.darkBg').click(function(){
   $(this).css({'display':'none'});
   $('.s_popupMenu').hide();
   $('.s_popupOrder').hide();
 });
-
 $('.s_tktMenu').click(function(){
   $('.s_popupMenu').show();
   $('.darkBg').css({'display':'block'});
 });
-
 $('.s_tktOrder').click(function(){
   $('.s_popupOrder').show();
   $('.darkBg').css({'display':'block'});
 });
-
 $('.s_popup > li').click(function(){
   $('.s_popupMenu').css({'display':'none'});
   $('.darkBg').css({'display':'none'});
@@ -197,14 +216,8 @@ $('.s_popup > li').click(function(){
   $('.s_popupOrder').hide();
 });
 
-  /**** 서브 메뉴 분야 선택 인기순 *****/
-
-
-
-
 
   /**** 서브 메뉴 레이아웃  *****/
-
   window.addEventListener("load", () => {
     //Masonry 레이아웃(Isotope 플러그인 이용)
     const iso = new Isotope(".s_tktList", {
@@ -213,7 +226,7 @@ $('.s_popup > li').click(function(){
     });
   
     const sTktListBtn = document.querySelectorAll(".s_popup > li"); //.btn>li들을 변수에
-  
+    
     for (let el of sTktListBtn) {
       //배열 filterBtn 의 아이템(갯수) 만큼 반복
       el.addEventListener("click", (e) => {
@@ -239,70 +252,25 @@ $('.s_popup > li').click(function(){
 /*** 예매 검색 필터 ***/
 
 
+/** 공연 정보 toggle **/
 
-const $searchInput = document.querySelector("article");
-const $wordList = document.querySelector("s_tktTxt");
+var toggle = {
+  click: function(target) {
+    var $target = $(target);
+    $target.on('click', function() {
 
-// 특수문자 처리
-const escapeRegExp = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+      if ($(this).hasClass('on')) {
+        slideUp($target);
+      } else {
+        slideUp($target);
+        $(this).addClass('on').next().slideDown(400);
+      }
 
-function ch2pattern(ch) {
-  // 사용자가 초성만 입력한 경우
-  if (/[ㄱ-ㅎ]/.test(ch)) {
-    const chToBegin = {
-      ㄱ: "가".charCodeAt(0),
-      ㄲ: "까".charCodeAt(0),
-      ㄴ: "나".charCodeAt(0),
-      ㄷ: "다".charCodeAt(0),
-      ㄸ: "따".charCodeAt(0),
-      ㄹ: "라".charCodeAt(0),
-      ㅁ: "마".charCodeAt(0),
-      ㅂ: "바".charCodeAt(0),
-      ㅃ: "빠".charCodeAt(0),
-      ㅅ: "사".charCodeAt(0),
-      ㅆ: "싸".charCodeAt(0),
-      ㅇ: "아".charCodeAt(0),
-      ㅈ: "자".charCodeAt(0),
-      ㅊ: "차".charCodeAt(0),
-      ㅋ: "카".charCodeAt(0),
-      ㅌ: "타".charCodeAt(0),
-      ㅍ: "파".charCodeAt(0),
-      ㅎ: "하".charCodeAt(0),
-    };
-    const begin = chToBegin[ch];
-    const end = begin + 587;
-    return `[${ch}\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
+      function slideUp($target) {
+        $target.removeClass('on').next().slideUp(400);
+      }
+
+    });
   }
-  // 사용자가 초성+중성 또는 초성+중성+종성을 입력한 경우
-  else if (/[가-히]/.test(ch)) {
-    const offset = "가".charCodeAt(0);
-    const chCode = ch.charCodeAt(0) - offset;
-    // 사용자가 초성+중성을 입력한 경우
-    if (chCode % 28 <= 0) {
-      const begin = Math.floor(chCode / 28) * 28 + offset;
-      const end = begin + 27;
-      return `[\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
-    }
-    // 사용자가 초성+중성+종성을 입력한 경우
-    else return ch;
-  }
-  // 한글이 입력되지 않은 경우
-  else return escapeRegExp(ch);
-}
-
-// 퍼지 문자열 검색을 위한 정규식 생성
-function createFuzzyMatcher(input) {
-  const pattern = input.split("").map(ch2pattern).join(".*?");
-  return new RegExp(pattern);
-}
-
-// 한글 퍼지 문자열 검색
-$searchInput.addEventListener("input", (e) => {
-  const query = e.target.value;
-  const regex = createFuzzyMatcher(query);
-  const words = $wordList.children;
-  for (let i = 0; i < words.length; i++) {
-    if (regex.test(words[i].textContent.toLowerCase())) words[i].removeClass("hide");
-    else words[i].addClass("hide");
-  }
-});
+};
+toggle.click('.infoBox');
