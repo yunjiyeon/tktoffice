a7f59c61ed50380ba5c52e4dd3bf37bc03c68435
yunjiyeon <yangdom@naver.com>
2022-11-30 16:25:00 +0900

    푸터수정

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@
 $(".btnSub").click(function () {
   $("#gnb").removeClass("hide");
   $("#gnb").animate({ right: 0, left: 0 }, 300, "swing"); // swing 속도감이 높아짐
+  $('.topBtn, .fixedBtn').addClass("hide");
 });
 
 $(".close").click(function () {
   $("#gnb").animate({ right: -700, left: 700 }, 300, "swing", function () {
     $(this).addClass("hide");
+    $('.topBtn, .fixedBtn').removeClass("hide");
   });
 });
 
@@ -17,7 +19,7 @@ let bannerSlide = {
   loop: true,
   autoplay: {
     //자동슬라이드 (false-비활성화)
-    delay: 2500, // 시간 설정
+    delay: 2500, // 시간 설정 
     disableOnInteraction: false, // false-스와이프 후 자동 재생
   },
 };
@@ -133,23 +135,29 @@ window.addEventListener("load", () => {
 //   $(".clickHeart").css({ display: "block" });
 // });
 
-/**** 동영상 실행  *****/
+/**** 동영상 정지  *****/
 $(".videoDarkBg, .videoClose").click(function () {
   $(".videoDarkBg").css({ display: "none" });
   $(".videoClose").css({ display: "none" });
   $(".videoPopup").hide();
+  $("#player")[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
 });
 
+/**** 동영상 실행  *****/
 $(".video").click(function () {
   $(".videoPopup").show();
   $(".videoDarkBg").css({ display: "block" });
   $(".videoClose").css({ display: "block" });
+  $("#player")[0].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
 });
+
+
+
 
 /*** topBtn ****/
 $(function () {
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 520) {
+    if ($(this).scrollTop() > 300) {
       $(".topBtn").addClass("topBtnActive").css("opacity", "0.85");
       $(".fixedBtn").addClass("fixedBtnActive");
     } else {
@@ -231,21 +239,29 @@ $(".darkBg").click(function () {
   $(this).css({ display: "none" });
   $(".s_popupMenu").hide();
   $(".s_popupOrder").hide();
+  $(".store_popupMenu").hide();
+  $(".store_popupOrder").hide();
 });
-$(".s_tktMenu").click(function () {
+$(".s_tktMenu, .s_storeMenu").click(function () {
   $(".s_popupMenu").show();
+  $(".store_popupMenu").show();
   $(".darkBg").css({ display: "block" });
+
 });
-$(".s_tktOrder").click(function () {
+$(".s_tktOrder, .s_storeOrder").click(function () {
   $(".s_popupOrder").show();
+  $(".store_popupOrder").show();
   $(".darkBg").css({ display: "block" });
 });
-$(".s_popup > li").click(function () {
+$(".s_popup > li, .store_popup > li").click(function () {
   $(".s_popupMenu").css({ display: "none" });
+  $(".store_popupMenu").css({ display: "none" });
   $(".darkBg").css({ display: "none" });
   $(".s_popupMenu").hide();
   $(".s_popupOrder").hide();
+  $(".store_popupOrder").hide();
 });
+
 
 /**** 서브 메뉴 레이아웃  *****/
 window.addEventListener("load", () => {
@@ -278,6 +294,38 @@ window.addEventListener("load", () => {
   }
 });
 
+/** 스토어 레이아웃 **/
+// window.addEventListener("load", () => {
+//   //Masonry 레이아웃(Isotope 플러그인 이용)
+//   const iso = new Isotope(".s_storeList", {
+//     // options
+//     itemSelector: "article",
+//   });
+
+//   const storeTktListBtn = document.querySelectorAll(".store_popup > li"); //.btn>li들을 변수에
+
+//   for (let el of storeTktListBtn) {
+//     //배열 filterBtn 의 아이템(갯수) 만큼 반복
+//     el.addEventListener("click", (e) => {
+//       e.preventDefault();
+
+//       //클릭을 할때 각 아이템(버튼)에 반복, on클라스 없애줌
+//       for (let el of sTktListBtn) {
+//         el.classList.remove("on");
+//       }
+
+//       //클릭한 버튼에 클라스 넣어줌
+//       e.currentTarget.classList.add("on");
+
+//       //클릭한 버튼에 있는 a태그 안의 속성 href의 value값을 가져온다
+//       const storeTkt = e.currentTarget.querySelector("a").getAttribute("href");
+
+//       iso.arrange({ filter: storeTkt }); //버튼을 누르면 필터링 작동(플러그인)
+//     });
+//   }
+// });
+
+
 /*** 예매 검색 필터 ***/
 
 /** 공연 정보 toggle **/
@@ -300,3 +348,37 @@ var toggle = {
   },
 };
 toggle.click(".infoBox");
+
+
+/**footer**/
+
+var footerToggle = {
+  click: function (target) {
+    var $target = $(target);
+    $target.on("click", function () {
+      if ($(this).hasClass("on")) {
+        slideUp($target);
+      } else {
+        slideUp($target);
+        $(this).addClass("on").next().slideDown(400);
+      }
+
+      function slideUp($target) {
+        $target.removeClass("on").next().slideUp(400);
+      }
+    });
+  },
+};
+
+
+footerToggle.click(".officeInfoBtn");
+
+
+$('.officeInfoBtn').click(function(){
+  $('.officeInfoBtn img').toggleClass('btnImg');
+});
+
+$('.infoBox').click(function(){
+  $(this).find('span img').toggleClass('btnImg');
+  $(this).siblings('.infoBox span img').slideToggle();
+});
